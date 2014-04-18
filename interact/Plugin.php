e537f5478886fdb18e87559061216c2dec72537c
Gerard van Helden <gerard@zicht.nl>
2014-04-18 13:47:10 +0000

    Copying svn://zon/projecten/zicht-libraries/z-plugins/trunk/interact to svn://zon/projecten/zicht-libraries/z-plugin-interact/trunk/interact
    git-svn-id: svn://zon/projecten/zicht-libraries/z-plugin-interact/trunk@35679 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ interact/Plugin.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @author Gerard van Helden <gerard@zicht.nl>
+ * @copyright Zicht online <http://zicht.nl>
+ */
+namespace Zicht\Tool\Plugin\Interact;
+
+use Zicht\Tool\Plugin as BasePlugin;
+use \Zicht\Tool\Container\Container;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+/**
+ * Provides utilities to interact with the user.
+ */
+class Plugin extends BasePlugin
+{
+    public function appendConfiguration(ArrayNodeDefinition $rootNode)
+    {
+    }
+
+    public function setContainer(Container $container)
+    {
+        $container->method(
+            'ask',
+            function($container, $q, $default = null) {
+                return $container->console_dialog_helper->ask(
+                    $container->output,
+                    $q . ($default ? sprintf(' [<info>%s</info>]', $default) : '') . ': ',
+                    $default
+                );
+            }
+        );
+        $container->method(
+            'choose',
+            function($container, $q, $options) {
+                foreach ($options as $key => $option) {
+                    $container->output->writeln(sprintf('[<info>%s</info>] %s', $key, $option));
+                }
+
+                return $container->console_dialog_helper->askAndValidate(
+                    $container->output,
+                    "$q: ",
+                    function($value) use($options) {
+                        if (!array_key_exists($value, $options)) {
+                            throw new \InvalidArgumentException("Invalid option [$value]");
+                        }
+                        return $options[$value];
+                    }
+                );
+            }
+        );
+
+        $container->method(
+            'confirm',
+            function($container, $q, $default = false) {
+                return $container->console_dialog_helper->askConfirmation(
+                    $container->output,
+                    $q .
+                        ($default === false ? ' [y/N] ' : ' [Y/n]'),
+                    $default
+                );
+            }
+        );
+    }
+}
